33f2baa9ad902485915b2d2dd6d3819583dbf71d
subhayu-qxlabai <subhayu@qxlabai.com>
2024-03-07 00:06:50 +0530

    feat: add __hash__ to Question, QuestionSplit

--- models/generic.py
+++ models/generic.py
@@ -2,7 +2,13 @@ from pydantic import BaseModel
 
 class Question(BaseModel):
     question: str
+    
+    def __hash__(self) -> int:
+        return hash(self.question)
 
 class QuestionSplit(Question):
     tasks: list[str]
     can_i_answer: bool
+
+    def __hash__(self) -> int:
+        return hash(self.question)
